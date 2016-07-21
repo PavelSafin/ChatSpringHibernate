851e8540d7c6bb4476a7dde5f6b783541e2c80ae
PavelSafin <pavel_safin@icloud.com>
2016-07-21 22:11:56 +0300

    Refactor Code

--- src/main/java/com/toster/chat/dao/UserDao.java
+++ src/main/java/com/toster/chat/dao/UserDao.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface UserDao {
     public User getUser(Long userId);
+
     public User save(User user);
+
     public List<User> getAllUsers();
+
     public void delete(Long userId);
+
     public User findUserByEmail(String email);
 }
--- src/main/java/com/toster/chat/service/MessageService.java
+++ src/main/java/com/toster/chat/service/MessageService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface MessageService {
     public ChatMessage getMessage(Long messageId);
+
     public List<ChatMessage> getRecentMessages(int limit, String nick1, String nick2);
+
     public void addMessage(ChatMessage message);
+
     public StringBuilder fetchChatHistory(int limit, String email1, String email2);
 }
